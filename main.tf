09f29738dfdfa461983b6458655df2935ec5bacb
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2026-05-02 13:30:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 terraform {
   required_providers {
     samsungcloudplatform = {
-      version = "3.16.1"
+      version = "3.18.0"
       source  = "SamsungSDSCloud/samsungcloudplatform"
     }
   }
